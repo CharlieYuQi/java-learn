a39338c8e5c53ff6cb6d0989e72d40170112d94e
YuQi <yuqi@cmhi.chinamobile.com>
2017-08-01 11:04:28 +0800

    add channelpool application in netty

--- nio/src/main/java/tk/yuqibit/nio/pool/NettyClientHander.java
+++ nio/src/main/java/tk/yuqibit/nio/pool/NettyClientHander.java
@@ -8,10 +8,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by YuQi on 2017/7/31.
  */
-public class HttpClientHander extends ChannelInboundHandlerAdapter {
-    static  AtomicInteger count  = new AtomicInteger(1);
+public class NettyClientHander extends ChannelInboundHandlerAdapter {
+    static AtomicInteger count = new AtomicInteger(1);
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println(count.getAndIncrement()+(String)msg);
+        System.out.println(count.getAndIncrement() + ":" + msg);
     }
 }
