7add7bdab3a1d47b29d13b2e8e22fc2c7ea9afee
YuQi <yuqi@cmhi.chinamobile.com>
2017-08-11 15:07:06 +0800

    add channelpool application in netty

--- nio/src/main/java/tk/yuqibit/nio/codec/protobuf/SubReqServer.java
+++ nio/src/main/java/tk/yuqibit/nio/codec/protobuf/SubReqServer.java
@@ -25,6 +25,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
@@ -36,52 +37,47 @@ import io.netty.handler.logging.LoggingHandler;
  */
 public class SubReqServer {
     public void bind(int port) throws Exception {
-	// 配置服务端的NIO线程组
-	EventLoopGroup bossGroup = new NioEventLoopGroup();
-	EventLoopGroup workerGroup = new NioEventLoopGroup();
-	try {
-	    ServerBootstrap b = new ServerBootstrap();
-	    b.group(bossGroup, workerGroup)
-		    .channel(NioServerSocketChannel.class)
-		    .option(ChannelOption.SO_BACKLOG, 100)
-		    .handler(new LoggingHandler(LogLevel.INFO))
-		    .childHandler(new ChannelInitializer<SocketChannel>() {
-			@Override
-			public void initChannel(SocketChannel ch) {
-			    // ch.pipeline().addLast(
-			    // new ProtobufVarint32FrameDecoder());
-			    ch.pipeline().addLast(
-				    new ProtobufDecoder(
-					    SubscribeReqProto.SubscribeReq
-						    .getDefaultInstance()));
-			    ch.pipeline().addLast(
-				    new ProtobufVarint32LengthFieldPrepender());
-			    ch.pipeline().addLast(new ProtobufEncoder());
-			    ch.pipeline().addLast(new SubReqServerHandler());
-			}
-		    });
+        // 配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+                            ch.pipeline().addLast(new ProtobufDecoder(
+                                    SubscribeReqProto.SubscribeReq.getDefaultInstance()));
+                            ch.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
+                            ch.pipeline().addLast(new ProtobufEncoder());
+                            ch.pipeline().addLast(new SubReqServerHandler());
+                        }
+                    });
 
-	    // 绑定端口，同步等待成功
-	    ChannelFuture f = b.bind(port).sync();
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
 
-	    // 等待服务端监听端口关闭
-	    f.channel().closeFuture().sync();
-	} finally {
-	    // 优雅退出，释放线程池资源
-	    bossGroup.shutdownGracefully();
-	    workerGroup.shutdownGracefully();
-	}
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
     public static void main(String[] args) throws Exception {
-	int port = 8080;
-	if (args != null && args.length > 0) {
-	    try {
-		port = Integer.valueOf(args[0]);
-	    } catch (NumberFormatException e) {
-		// 采用默认值
-	    }
-	}
-	new SubReqServer().bind(port);
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        new SubReqServer().bind(port);
     }
 }
