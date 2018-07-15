2481e6c0868293657b00731e767122f70633649f
zhizhou.yq <zhizhou.yq@cainiao.com>
2018-07-15 23:05:14 +0800

    add fl

---
+++ tools/src/main/java/tk/yuqi/tools/tools/ToolsApplication.java
@@ -0,0 +1,12 @@
+package tk.yuqi.tools.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsApplication.class, args);
+	}
+}
