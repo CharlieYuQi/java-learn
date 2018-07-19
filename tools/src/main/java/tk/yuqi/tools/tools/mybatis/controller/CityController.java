21ebed299021e1d590ce29f6ab712688b8639a1b
zhizhou.yq <zhizhou.yq@cainiao.com>
2018-07-19 22:22:48 +0800

    add city

--- tools/src/main/java/tk/yuqi/tools/tools/mybatis/controller/CityController.java
+++ tools/src/main/java/tk/yuqi/tools/tools/mybatis/controller/CityController.java
@@ -3,7 +3,9 @@ package tk.yuqi.tools.tools.mybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tk.yuqi.tools.tools.mybatis.dao.CityDao;
 import tk.yuqi.tools.tools.mybatis.domain.City;
@@ -22,5 +24,11 @@ public class CityController {
     @RequestMapping(value="/getCityByState/{state}")
     public City getCityByState( @PathVariable String state){
         return cityDao.findByState(state);
+    }
+
+    @RequestMapping(value="city",method = RequestMethod.POST)
+    public long addCity(@RequestBody City city) throws Exception {
+        cityDao.insert(city);
+        return city.getId();
     }
 }
