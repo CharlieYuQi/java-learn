7580160cfceca398a67e3a80c29e37df024d2703
zhizhou.yq <zhizhou.yq@cainiao.com>
2018-07-19 12:20:58 +0800

    add mybatis

--- tools/src/main/java/tk/yuqi/tools/tools/mybatis/controller/CityController.java
+++ tools/src/main/java/tk/yuqi/tools/tools/mybatis/controller/CityController.java
@@ -1,12 +1,4 @@
-/*
- * Copyright 1999-2015 Alibaba.com All right reserved. This software is the
- * confidential and proprietary information of Alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Alibaba.com.
- * @author zhizhou
- * @date 2018/07/19
- */
+
 package tk.yuqi.tools.tools.mybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +11,6 @@ import tk.yuqi.tools.tools.mybatis.domain.City;
 /**
  * 类 CityController 的实现描述：CityController
  *
- * @author <a href="mailto:zhizhou.yq@cainiao.com">知周</a>
  * @since 2018/7/19
  */
 @RestController
