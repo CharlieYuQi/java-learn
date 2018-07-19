48ad819bf3674e532f1d983ee05fd756e421537a
zhizhou.yq <zhizhou.yq@cainiao.com>
2018-07-19 12:20:58 +0800

    add mybatis

--- tools/src/main/java/tk/yuqi/tools/tools/rest/Greeting.java
+++ tools/src/main/java/tk/yuqi/tools/tools/rest/Greeting.java
@@ -1,12 +1,4 @@
-/*
- * Copyright 1999-2015 Alibaba.com All right reserved. This software is the
- * confidential and proprietary information of Alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Alibaba.com.
- * @author zhizhou
- * @date 2018/07/19
- */
+
 package tk.yuqi.tools.tools.rest;
 
 import lombok.Data;
@@ -14,7 +6,6 @@ import lombok.Data;
 /**
  * 类 Greeting 的实现描述：Greeting
  *
- * @author <a href="mailto:zhizhou.yq@cainiao.com">知周</a>
  * @since 2018/7/19
  */
 @Data
